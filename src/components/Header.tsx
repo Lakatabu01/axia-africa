098c9afe8fa989d8df77258f3100027d71d8d994
Olanrewaju <lanrebamgbade@gmail.com>
2023-11-15 05:50:46 -0800

    create apply button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 import logo from "../assets/axia-logo.svg";
 import NavButton from "./nav-btn";
+import ApplyButton from "./apply-btn";
 //type Props = {}
 
 function Header() {
   return (
-    <div className="bg-green-200 w-full flex justify-center p-2">
-      <div>
-        <img src={logo} alt="Axia logo" />
-      </div>
+    <div className="bg-green-200 w-full justify-center flex py-8 p-4 items-center">
+      <div className="justify-between flex  items-center w-4/5 ">
+        <div>
+          <img src={logo} alt="Axia logo" />
+        </div>
 
-      <nav className="flex justify-around p-2 w-2/4 ">
-        <div>
-          <NavButton content="About" />
-        </div>
-        <div>
-          <NavButton content="Schools" />
-        </div>
-        <div>
-          <NavButton content="Join our community" />
-        </div>
-        <div>
-          <NavButton content="Help" />
-        </div>
-      </nav>
+        <nav className="flex justify-around p-2 w-2/4 mx-8 ">
+          <div>
+            <NavButton content="About" />
+          </div>
+          <div>
+            <NavButton content="Schools" />
+          </div>
+          <div>
+            <NavButton content="Join our community" />
+          </div>
+          <div>
+            <NavButton content="Help" />
+          </div>
+        </nav>
+
+        <ApplyButton />
+      </div>
     </div>
   );
 }
--- src/components/apply-btn.tsx
+++ src/components/apply-btn.tsx
@@ -1,7 +1,13 @@
+import React from "react";
 const ApplyButton = () => {
   return (
     <div>
-      <button></button>
+      <button className="bg-transparent text-base leading-6 px-4 py-2 border-4 border-black">
+        {" "}
+        Apply now
+      </button>
     </div>
   );
 };
+
+export default ApplyButton;
